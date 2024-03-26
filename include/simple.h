24fccff1a2f2a21589754950d28fd8742d6f14bd
Markus Mertama <markus.mertama@insta.fi>
2024-03-26 13:19:44 +0200

    WASM build

--- include/simple.h
+++ include/simple.h
@@ -94,4 +94,40 @@ private:
     size_t m_position = 0;
     unsigned m_found = 0;
 };
+
+class NeuropiaEnv {
+public:
+    NeuropiaEnv(const NeuropiaEnv&) = delete;
+    NeuropiaEnv& operator=(const NeuropiaEnv&) = delete;
+    NeuropiaEnv(const std::string& root) :  m_root(root) {
+        m_params.addHelp( topologyRe, "\',\'-separated list of integers");
+        m_params.addHelp( activationFunctionRe, "\',\'-separated list of activation functions: \"sigmoid, relu or elu\"");
+        m_params.addHelp( dropoutRateRe, "\',\'-separated of list of real numbers");
+    }
+    virtual ~NeuropiaEnv();
+    void setLogger(std::function<void (const std::string&)> logger) {
+        if(m_prevStreamBufCerr) {
+            std::cerr.rdbuf(m_prevStreamBufCerr);
+        }
+        if(m_prevStreamBufCout) {
+            std::cout.rdbuf(m_prevStreamBufCout);
+        }
+        m_logStream = std::make_unique<NeuropiaSimple::SimpleLogStream>(logger);
+        m_prevStreamBufCout = std::cout.rdbuf(m_logStream.get());
+        m_prevStreamBufCerr = std::cerr.rdbuf(m_logStream.get());
+    }
+    Neuropia::Layer m_network = {};
+    Neuropia::Params m_params = {
+        DEFAULT_PARAMS
+};
+    const std::string m_root;
+    std::unique_ptr<SimpleLogStream> m_logStream = {};
+    std::streambuf* m_prevStreamBufCout = nullptr;
+    std::streambuf* m_prevStreamBufCerr = nullptr;
+    bool m_once = false;
+
+    std::unique_ptr<SimpleTrainer> m_trainer = {};
+    std::unique_ptr<SimpleVerifier> m_verifier = {};
+};
+
 }
